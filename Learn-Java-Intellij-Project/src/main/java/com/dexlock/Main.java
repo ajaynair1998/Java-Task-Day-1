8c023d0581b7c009549b500ffe49d8cce00e94dc
ajay <ajaynairarun@gmail.com>
2021-09-10 13:52:30 +0530

    testing package importing

---
+++ Learn-Java-Intellij-Project/src/main/java/com/dexlock/Main.java
@@ -0,0 +1,13 @@
+package com.dexlock;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        task.Test t=new task.Test(3);
+        System.out.println(t.returnData());
+    }
+
+
+}
+
---
+++ Learn-Java-Intellij-Project/src/main/java/task/Test.java
@@ -0,0 +1,14 @@
+package task;
+
+public class Test{
+    int number;
+    public Test(int i){
+        System.out.println("Test class");
+        this.number=i;
+
+    }
+
+    public int returnData(){
+        return this.number;
+    }
+}
